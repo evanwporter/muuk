c8b80274d45d0313807a68ac95ea2642958fd4e0
Evan Porter <115374841+evanwporter@users.noreply.github.com>
2025-03-23 09:48:46 -0700

    replace `tl::expected` with `Result`

--- include/muukbuilder.h
+++ include/muukbuilder.h
@@ -4,33 +4,29 @@
 #include <memory>
 #include <string>
 
-#include <spdlog/spdlog.h>
-#include <tl/expected.hpp>
-
 #include "buildbackend.hpp"
 #include "muukfiler.h"
 #include "muuklockgen.h"
+#include "rustify.hpp"
 
 class MuukBuilder {
 public:
     explicit MuukBuilder(MuukFiler& config_manager);
 
-    tl::expected<void, std::string> build(std::string& target_build, const std::string& compiler, const std::string& profile);
-    tl::expected<bool, std::string> is_compiler_available() const;
+    Result<void> build(std::string& target_build, const std::string& compiler, const std::string& profile);
+    Result<bool> is_compiler_available() const;
 
 private:
     MuukFiler& config_manager_;
     std::unique_ptr<NinjaBackend> ninja_backend_;
     std::unique_ptr<CompileCommandsBackend> compdb_backend_;
 
-    std::shared_ptr<spdlog::logger> logger_;
-
-    tl::expected<void, std::string> execute_build(const std::string& profile) const;
-    tl::expected<muuk::Compiler, std::string> detect_default_compiler() const;
+    Result<void> execute_build(const std::string& profile) const;
+    Result<muuk::Compiler> detect_default_compiler() const;
     std::string detect_archiver(muuk::Compiler compiler) const;
     std::string detect_linker(muuk::Compiler compiler) const;
-    tl::expected<std::string, std::string> select_profile(const std::string& profile);
-    tl::expected<void, std::string> add_script(const std::string& profile, const std::string& build_name);
+    Result<std::string> select_profile(const std::string& profile);
+    Result<void> add_script(const std::string& profile, const std::string& build_name);
 
     void generate_compile_commands(const std::string& profile);
 };
