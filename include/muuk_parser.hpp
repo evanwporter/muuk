3fbdfdaa4745c7cda70fae0d87f072ee7883e01f
Evan <115374841+evanwporter@users.noreply.github.com>
2025-03-30 09:16:19 -0700

    Update muuk_parser.hpp

--- include/muuk_parser.hpp
+++ include/muuk_parser.hpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <unordered_set>
 #include <vector>
+#include <type_traits>
 
 #include <toml.hpp>
 
@@ -21,4 +22,17 @@ namespace muuk {
         const std::string& key,
         const std::string& prefix = "");
 
+    template <typename T>
+    T find_or(const toml::value& v, const std::string& key, const T& default_val) {
+        if (!v.contains(key)) return default_val;
+        return toml::find_or<T>(v, key, default_val);
+    }
+    
+    // Specialization for std::unordered_set<T>
+    template <typename T>
+    std::unordered_set<T> find_or(const toml::value& v, const std::string& key, const std::unordered_set<T>& default_val) {
+        std::vector<T> default_vec(default_val.begin(), default_val.end());
+        std::vector<T> vec = toml::find_or(v, key, default_vec);
+        return std::unordered_set<T>(vec.begin(), vec.end());
+    }
 } // namespace muuk
