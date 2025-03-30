e6b3ea4910f46764d7c4f3cb476a7e881106dceb
Evan <115374841+evanwporter@users.noreply.github.com>
2025-03-30 08:58:24 -0700

    Update moduleresolver.hpp

--- include/moduleresolver.hpp
+++ include/moduleresolver.hpp
@@ -3,5 +3,17 @@
 #include "buildmanager.h"
 
 namespace muuk {
+    void generate_compilation_database(
+        BuildManager& build_manager, 
+        const std::string& dependency_db, 
+        const std::string& build_dir);
+    nlohmann::json parse_dependency_db(const std::string& dependency_db);
+    void resolve_provided_modules(
+        const nlohmann::json& dependencies,
+        std::unordered_map<std::string, CompilationTarget*>& target_map);
+    void resolve_required_modules(
+        const nlohmann::json& dependencies,
+        BuildManager& build_manager,
+        std::unordered_map<std::string, CompilationTarget*>& target_map);
     void resolve_modules(BuildManager& build_manager, const std::string& build_dir);
 } // namespace muuk
